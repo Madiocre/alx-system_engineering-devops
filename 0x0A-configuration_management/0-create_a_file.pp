b1f8cf5466021bff9db054fee05d9624460f8f40
Madiocre <madiocre.x@gmail.com>
2024-03-22 07:32:13 -0700

    puppet w/ issue

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# This Creates my first puppet file
+file { '/tmp/school':
+  ensure  => present,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
+
